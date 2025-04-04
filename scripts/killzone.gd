3b020bbcc4de78838f19b7e7e6b44baa0302becd
Pahril <88573655+visoredkon@users.noreply.github.com>
2025-04-04 16:11:03 +0800

    feat: collectible coin and killzone

---
+++ scripts/killzone.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(_body: Node2D) -> void:
+    timer.start()
+
+func _on_timer_timeout() -> void:
+    get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,30 +6,30 @@ const JUMP_VELOCITY = -400.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("ui_left", "ui_right")
+    var direction := Input.get_axis("ui_left", "ui_right")
 
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("idle")
-		else:
-			animated_sprite_2d.play("run")
-	else:
-		animated_sprite_2d.play("jump")
+    if is_on_floor():
+        if direction == 0:
+            animated_sprite_2d.play("idle")
+        else:
+            animated_sprite_2d.play("run")
+    else:
+        animated_sprite_2d.play("jump")
 
-		velocity += get_gravity() * delta
+        velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
 
-	if direction:
-		match direction:
-			1.0:
-				animated_sprite_2d.flip_h = false
-			-1.0:
-				animated_sprite_2d.flip_h = true
+    if direction:
+        match direction:
+            1.0:
+                animated_sprite_2d.flip_h = false
+            -1.0:
+                animated_sprite_2d.flip_h = true
 
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+        velocity.x = direction * SPEED
+    else:
+        velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+    move_and_slide()
